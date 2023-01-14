43582f3bb5dbebea9918c3475b66e85d6785bc57
shuurkaaaa <114067980+shuurkaaaa@users.noreply.github.com>
2023-01-14 05:35:02 +0200

    Add files via upload

---
+++ sem1_Lab1/Task4_Lab1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Task4_Lab1
+{
+    class Program
+    {
+        static double Pow(double x, double n)
+        {
+            double result = x;
+            if (n == 0) return 1;
+            for (int j = 1; j < n; j++)
+                result *= x;
+            return result;
+        }
+
+        static double Fac(double n)
+        {
+            double factorial = 1;
+            for (int j = 1; j <= n; j++)
+                factorial *= j;
+            return factorial;
+        }
+        static double Sin(double val)
+        {
+            while (val > 3.14159265359 * 2)
+            {
+                val -= 3.14159265359 * 2;
+            }
+            double result = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                double a = Pow(-1, i);
+                double x = Pow(val, (2 * i) + 1);
+
+                double f = Fac((2 * i) + 1);
+
+                result += (a * x / f);
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input number: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("With Mathematics Library: " + Math.Sin(a));
+                Console.WriteLine("Without using a library: " + Sin(a));
+            }
+
+        }
+    }
+}
