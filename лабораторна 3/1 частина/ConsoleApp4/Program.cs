18be10b63581dec09d8dbf2121d2a2a96506ed81
shuurkaaaa <114067980+shuurkaaaa@users.noreply.github.com>
2023-06-16 00:56:14 +0300

    Add files via upload

---
+++ лабораторна 3/1 частина/ConsoleApp4/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Deque<Item> : IEnumerable<Item>
+{
+    private Node first;
+    private Node last;
+    private int count;
+
+    private class Node
+    {
+        public Item item;
+        public Node next;
+        public Node previous;
+    }
+
+    public Deque()
+    {
+        first = null;
+        last = null;
+        count = 0;
+    }
+
+    public bool IsEmpty()
+    {
+        return count == 0;
+    }
+
+    public int Size()
+    {
+        return count;
+    }
+
+    public void AddFirst(Item item)
+    {
+        if (item == null)
+            throw new ArgumentNullException();
+
+        Node newNode = new Node
+        {
+            item = item,
+            next = first,
+            previous = null
+        };
+
+        if (IsEmpty())
+        {
+            first = newNode;
+            last = newNode;
+        }
+        else
+        {
+            first.previous = newNode;
+            first = newNode;
+        }
+
+        count++;
+    }
+
+    public void AddLast(Item item)
+    {
+        if (item == null)
+            throw new ArgumentNullException();
+
+        Node newNode = new Node
+        {
+            item = item,
+            next = null,
+            previous = last
+        };
+
+        if (IsEmpty())
+        {
+            first = newNode;
+            last = newNode;
+        }
+        else
+        {
+            last.next = newNode;
+            last = newNode;
+        }
+
+        count++;
+    }
+
+    public Item RemoveFirst()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Deque is empty");
+
+        Item item = first.item;
+        first = first.next;
+        count--;
+
+        if (IsEmpty())
+            last = null;
+        else
+            first.previous = null;
+
+        return item;
+    }
+
+    public Item RemoveLast()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Deque is empty");
+
+        Item item = last.item;
+        last = last.previous;
+        count--;
+
+        if (IsEmpty())
+            first = null;
+        else
+            last.next = null;
+
+        return item;
+    }
+
+    public IEnumerator<Item> GetEnumerator()
+    {
+        Node current = first;
+
+        while (current != null)
+        {
+            yield return current.item;
+            current = current.next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Test the Deque implementation
+        Deque<int> deque = new Deque<int>();
+
+        deque.AddFirst(1);
+        deque.AddFirst(2);
+        deque.AddLast(3);
+        deque.AddLast(4);
+
+        foreach (int item in deque)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("Size: " + deque.Size());
+
+        int removedItem = deque.RemoveFirst();
+        Console.WriteLine("Removed item: " + removedItem);
+
+        foreach (int item in deque)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ лабораторна 3/2 частина/ConsoleApp5/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomizedQueue<Item> : IEnumerable<Item>
+{
+    private Item[] array;
+    private int count;
+    private Random random;
+
+    public RandomizedQueue()
+    {
+        array = new Item[2];
+        count = 0;
+        random = new Random();
+    }
+
+    public bool IsEmpty()
+    {
+        return count == 0;
+    }
+
+    public int Size()
+    {
+        return count;
+    }
+
+    public void Enqueue(Item item)
+    {
+        if (item == null)
+            throw new ArgumentNullException();
+
+        if (count == array.Length)
+            ResizeArray(2 * array.Length);
+
+        array[count++] = item;
+    }
+
+    public Item Dequeue()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Randomized queue is empty");
+
+        int randomIndex = random.Next(count);
+        Item item = array[randomIndex];
+
+        array[randomIndex] = array[count - 1];
+        array[count - 1] = default(Item);
+
+        count--;
+
+        if (count > 0 && count == array.Length / 4)
+            ResizeArray(array.Length / 2);
+
+        return item;
+    }
+
+    public Item Sample()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Randomized queue is empty");
+
+        int randomIndex = random.Next(count);
+        return array[randomIndex];
+    }
+
+    public IEnumerator<Item> GetEnumerator()
+    {
+        Item[] randomizedArray = new Item[count];
+        Array.Copy(array, randomizedArray, count);
+        Shuffle(randomizedArray);
+
+        foreach (Item item in randomizedArray)
+        {
+            yield return item;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    private void ResizeArray(int capacity)
+    {
+        Item[] newArray = new Item[capacity];
+        Array.Copy(array, newArray, count);
+        array = newArray;
+    }
+
+    private void Shuffle(Item[] items)
+    {
+        int n = items.Length;
+        for (int i = 0; i < n; i++)
+        {
+            int randomIndex = i + random.Next(n - i);
+            Item temp = items[randomIndex];
+            items[randomIndex] = items[i];
+            items[i] = temp;
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Test the RandomizedQueue implementation
+        RandomizedQueue<int> randomizedQueue = new RandomizedQueue<int>();
+
+        randomizedQueue.Enqueue(1);
+        randomizedQueue.Enqueue(2);
+        randomizedQueue.Enqueue(3);
+        randomizedQueue.Enqueue(4);
+
+        foreach (int item in randomizedQueue)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("Size: " + randomizedQueue.Size());
+
+        int removedItem = randomizedQueue.Dequeue();
+        Console.WriteLine("Removed item: " + removedItem);
+
+        foreach (int item in randomizedQueue)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
