bed1e5cbd87e57484a2706ad4d765986549fcb66
shuurkaaaa <114067980+shuurkaaaa@users.noreply.github.com>
2023-06-16 00:51:16 +0300

    Add files via upload

---
+++ завдання 1 лаб 2/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запитуємо дані від користувача
+        Console.WriteLine("Введіть ваше прізвище:");
+        string прізвище = Console.ReadLine();
+
+        Console.WriteLine("Введіть вашу зарплату:");
+        decimal зарплата = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Введіть утримання:");
+        decimal утримання = Convert.ToDecimal(Console.ReadLine());
+
+        // Обчислюємо значення S (видано)
+        decimal видано = зарплата - утримання;
+
+        // Виводимо результат
+        Console.WriteLine("Значення S (видано): " + видано);
+    }
+}
---
+++ завдання 3 лаб 1,/задание 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string inputFilename = "words.txt";
+        string outputFilename = "sorted_words.txt";
+
+        // Зчитуємо слова зі вхідного файлу
+        string[] words = File.ReadAllLines(inputFilename);
+
+        // Сортуємо слова за алфавітом
+        Array.Sort(words);
+
+        // Записуємо відсортовані слова у вихідний файл
+        File.WriteAllLines(outputFilename, words);
+
+        Console.WriteLine("Words have been sorted and written to the file: " + outputFilename);
+    }
+}
+
+
